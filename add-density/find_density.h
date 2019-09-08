e76ed529858c5081bf0cd30d715a7a8d5f8df17f
KhusainovAydar <aikhusainov@edu.hse.ru>
2019-09-08 16:13:36 +0300

    resolve problems with compilation and add functions in find_density

---
+++ add-density/find_density.h
@@ -0,0 +1,21 @@
+#include <math.h>
+#define VCL_FASTEXP
+#include "vectorclass/vectormath_exp.h"
+#include "vectorclass/vectorclass.h"
+
+#define M_PI 3.14159265358979323846264338327950288
+#define one_over_sqrt_two_pi 0.398942280401432702863218082712
+
+
+template<typename T>
+static inline T find_density_0(T &x, T &mean, T &deviation)
+
+
+template<typename T>
+static inline T find_density_1(T &x, T &mean, T &deviation)
+
+
+template<typename T>
+static inline T find_density_2(T &x, T &mean, T &deviation)
+
+
